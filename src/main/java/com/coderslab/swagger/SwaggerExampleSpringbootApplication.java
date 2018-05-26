daea2e790e7313db3d3cea1053de17c4bdac1661
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2018-05-27 04:01:06 +0600

    first commit

---
+++ src/main/java/com/coderslab/swagger/SwaggerExampleSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.coderslab.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExampleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExampleSpringbootApplication.class, args);
+	}
+}
